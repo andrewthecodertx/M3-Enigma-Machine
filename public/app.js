ee94da7088d92185d4e391088db614d929357478
Andrew S Erwin <andrewerwin73@gmail.com>
2026-01-01 16:35:39 -0600

    change url and port

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ class EnigmaUI {
     this.currentRotorPositions = [0, 0, 0]; // [left, middle, right]
     this.initialRotorPositions = [0, 0, 0];
     this.API_URL = this.isProduction
-      ? "https://andrewthecoder.com/enigmaserver"
+      ? "https://andrewthecoder.com:4444"
       : `http://${window.location.hostname}:4000`;
 
     this.initializeElements();
